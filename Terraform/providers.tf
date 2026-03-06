b4c8a7dc1f79ca606f331ec37cc1eb527437e2c1
santhoshS-awaninfotech <santhosh.sampath@awaninfotech.com>
2026-03-06 13:43:02 +0530

    Add Azure backend configuration to providers.tf
    Configure Azure backend for Terraform state management.

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = ">=1.0"
 
+  backend "azurerm" {
+    resource_group_name  = "RG-RnD-MAIN"
+    storage_account_name = "stawan"
+    container_name       = "sant"
+    key                  = "terraform.tfstate"
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
