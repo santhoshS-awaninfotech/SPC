22b8a593024da92bb8a17488e5e183e37f287121
santhoshS-awaninfotech <santhosh.sampath@awaninfotech.com>
2026-03-06 20:58:51 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,57 +1,4 @@
-# Create a Resource Group
-# resource "azurerm_resource_group" "SRG1" {
-#   name     = "RG_SANT_DELETE1"
-#   location = var.location
-#   tags = var.common_tags
-
-
-# }
-
-# # Create a Resource Group
-# resource "azurerm_resource_group" "SRG2" {
-#   name     = "RG_SANT_DELETE2"
-#   location = "West US2"
-#   tags = merge(
-#     var.common_tags,
-#     {
-#       Environment = "Testing"   # override just this one
-#       Name        = "specialRG"
-#     }
-#   )
-# }
-
-# resource "azurerm_resource_group" "rg" {
-#   count    = var.rg_count
-#   name     = "RG_SANT_SAMPLE${count.index + 1}"
-#   location = var.location
-#   tags     = var.common_tags
-# }
-
-
-# resource "azurerm_virtual_network" "VNET1" {
-#   name                = "SANT_VNET1"
-#   location            = azurerm_resource_group.SRG1.location
-#   resource_group_name = azurerm_resource_group.SRG1.name
-#   address_space       = ["10.20.0.0/22"]
-#   tags = var.common_tags
-# }
-
-# resource "azurerm_subnet" "SUBN1" {
-#   name                 = "sant_subnet1"
-#   resource_group_name  = azurerm_resource_group.SRG1.name
-#   virtual_network_name = azurerm_virtual_network.VNET1.name
-#   address_prefixes     = ["10.20.1.0/24"]
-# }
-
-# resource "azurerm_subnet" "SUBN2" {
-#   name                 = "sant_subnet2"
-#   resource_group_name  = azurerm_resource_group.SRG1.name
-#   virtual_network_name = azurerm_virtual_network.VNET1.name
-#   address_prefixes     = ["10.20.2.0/24"]
-
-# }
-
-#-----------------------------------
+#This is to create VM in Azure with required settings
 
 resource "azurerm_resource_group" "spcrg" {
   name     = "RG_SANT_TEMP"
@@ -166,7 +113,6 @@ resource "azurerm_windows_virtual_machine" "spcvm" {
     name                 = "osdisk-VM-SANT-SPC-SPOT1"
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
-    disk_size_gb         = 60
   }
 
   source_image_reference {
@@ -178,7 +124,6 @@ resource "azurerm_windows_virtual_machine" "spcvm" {
 
   priority        = "Spot"
   eviction_policy = "Deallocate"
-  #max_price = -1
 
   # provisioner "remote-exec" {
   #   inline = [
