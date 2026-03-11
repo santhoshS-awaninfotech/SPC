e7a148bd926a45c6e8298297cb122045f9d4692b
Santhosh Sampath <Santhosh.Sampath@ivanti.com>
2026-03-11 18:37:08 +0530

    updated main.tf

--- Terraform/scripts/boot.ps1
+++ Terraform/scripts/boot.ps1
@@ -1,22 +1,22 @@
 #boot script
 
-# Create users
+#1. Create users
 net user userA $env:USERA_PASSWORD /add
 net user userB $env:USERB_PASSWORD /add
 net localgroup Administrators userA /add
 net localgroup Administrators userB /add
 
-# Install Python
+#2. Install Python
 Invoke-WebRequest -Uri https://www.python.org/ftp/python/3.11.6/python-3.11.6-amd64.exe -OutFile C:\python-installer.exe
 Start-Process C:\python-installer.exe -ArgumentList '/quiet InstallAllUsers=1 PrependPath=1' -Wait
 
-# Install PostgreSQL via winget
+#3.1 Install PostgreSQL via winget
 winget install --id PostgreSQL.PostgreSQL.16 `
     --source winget `
     --accept-source-agreements `
     --accept-package-agreements -e
 
-# Set PostgreSQL Superuser password non-interactively
+#3.2 Set PostgreSQL Superuser password non-interactively
 $pgBinPath  = "C:\Program Files\PostgreSQL\16\bin"
 $pgDataPath = "C:\Program Files\PostgreSQL\16\data"
      
