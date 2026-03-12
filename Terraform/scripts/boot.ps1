dc646b25004de260d0ee319df0a23c83d63bfa83
Santhosh Sampath <Santhosh.Sampath@ivanti.com>
2026-03-12 17:20:35 +0530

    updated main.tf

--- Terraform/scripts/boot.ps1
+++ Terraform/scripts/boot.ps1
@@ -1,15 +1,20 @@
 #boot script
 
 #1. Create users
-net user userA $env:USERA_PASSWORD /add;
-net user userB $env:USERB_PASSWORD /add;
-net localgroup Administrators userA /add;
-net localgroup Administrators userB /add;
+if (-not (Get-LocalUser -Name "userA" -ErrorAction SilentlyContinue)) {
+net user userA $env:USERA_PASSWORD /add; net localgroup Administrators userA /add; 
+}
+if (-not (Get-LocalUser -Name "userB" -ErrorAction SilentlyContinue)) {
+net user userB $env:USERB_PASSWORD /add; net localgroup Administrators userB /add;
+}
 
 #2. Install Python
+if (-not (Get-Command python -ErrorAction SilentlyContinue)) {
 winget install --id Python.Python.3 --source winget --accept-source-agreements --accept-package-agreements -e;
+}
 
 #3.1 Install PostgreSQL
+if (-not (Get-Service -Name "postgresql-x64-16" -ErrorAction SilentlyContinue)) {
 winget install --id PostgreSQL.PostgreSQL.16 --source winget --accept-source-agreements --accept-package-agreements -e;
 
 #3.2 Set PostgreSQL Superuser password non-interactively
@@ -24,3 +29,4 @@ Restart-Service postgresql-x64-16;
 
 (Get-Content $pgHbaPath) -replace "trust","scram-sha-256" | Set-Content $pgHbaPath;
 Restart-Service postgresql-x64-16;
+}
