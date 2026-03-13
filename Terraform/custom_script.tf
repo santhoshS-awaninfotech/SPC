61d26c7b494914943086babbabda36eb56c74b00
Santhosh Sampath <Santhosh.Sampath@ivanti.com>
2026-03-13 15:08:08 +0530

    added

--- Terraform/custom_script.tf
+++ Terraform/custom_script.tf
@@ -15,7 +15,7 @@ resource "azurerm_virtual_machine_extension" "cscrp" {
 
   settings = <<SETTINGS
     {
-      "fileUris": ["https://stawan.blob.core.windows.net/sant/boot.ps1?sp=r&st=2026-03-12T09:59:09Z&se=2026-03-12T18:14:09Z&spr=https&sv=2024-11-04&sr=b&sig=htcgwLK0Ph2WkfyWmluoSof12YS%2FNLFrefo06zNBvPA%3D"],
+      "fileUris": ["https://stawan.blob.core.windows.net/sant/boot.ps1?sp=r&st=2026-03-13T09:21:53Z&se=2026-03-14T17:36:53Z&spr=https&sv=2024-11-04&sr=b&sig=A%2BC30D4SWsFX5s8K6tk6q5Oehc85hasAePEGN%2BSANoo%3D"],
       "commandToExecute": "powershell -ExecutionPolicy Unrestricted -File boot.ps1"
     }
   SETTINGS
