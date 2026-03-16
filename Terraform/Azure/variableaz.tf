bb98e08a9c156d1d33772566ea395918f71f22e3
Santhosh Sampath <Santhosh.Sampath@ivanti.com>
2026-03-16 19:49:58 +0530

    updated for aws and azure

--- Terraform/Azure/variableaz.tf
+++ Terraform/Azure/variableaz.tf
@@ -1,3 +1,37 @@
+
+variable "cloud" {
+  description = "Target cloud provider"
+  type        = string
+}
+
+#AWS
+variable "region" {
+ type    = string
+ default = "ap-south-1"
+}
+variable "availability_zone" {
+ type    = string
+ default = "ap-south-1a"
+}
+variable "instance_type" {
+ type    = string
+ default = "t3.micro"
+}
+
+#Azure
+variable "location" {
+  type    = string
+  default = "westus2"
+}
+variable "common_tags" {
+  type = map(string)
+  default = {
+    Environment = "DEV"
+    Owner       = "UserA"
+  }
+}
+
+#VM
 variable "admin_password" {
   description = "Admin password for the VM"
   type        = string
