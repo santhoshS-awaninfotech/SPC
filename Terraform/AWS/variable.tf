c852619f81282b59502aa5ed9879f50fa52233ea
Santhosh Sampath <Santhosh.Sampath@ivanti.com>
2026-03-19 20:39:48 +0530

    updated for aws 2 vms

--- Terraform/AWS/variable.tf
+++ Terraform/AWS/variable.tf
@@ -13,6 +13,10 @@ variable "region" {
 #  type    = string
 #  default = ""
 # }
+variable "keypair_name" {
+ type    = string
+ default = "sant-key-pair"
+}
 
 data "aws_availability_zones" "available" {
   state = "available"
