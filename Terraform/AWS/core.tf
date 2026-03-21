5e5c3f051a559b302e44356ff1f6cb83c92b1ef9
Santhosh Sampath <Santhosh.Sampath@ivanti.com>
2026-03-21 14:02:14 +0530

    updated for aws 2 vms

--- Terraform/AWS/core.tf
+++ Terraform/AWS/core.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 # VPC
 resource "aws_vpc" "spcvpc" {
   cidr_block = var.vpc_cidr
