c487dc7701c16d4bcaf369ecdfc619b2fc9848d3
Santhosh Sampath <Santhosh.Sampath@ivanti.com>
2026-03-23 20:49:17 +0530

    updated for multi region

--- Terraform/AWS/core.tf
+++ Terraform/AWS/core.tf
@@ -13,12 +13,26 @@ resource "aws_vpc" "spcvpc" {
   tags       = merge(var.common_tags, { Name = "VPC-${var.reg_code}-SPC"})
 }
 
+# resource "aws_subnet" "discsubnet" {
+#   vpc_id            = aws_vpc.spcvpc.id
+#   cidr_block        = var.discsubnet_cidr
+#   availability_zone = data.aws_availability_zones.available.names[0]
+#   tags   = merge(var.common_tags, { Name = "SUBNET-${var.reg_code}-SPC-STG-RUNR"})
+# }
+
+
+# Create multiple subnets, one per AZ up to the count you specify
 resource "aws_subnet" "discsubnet" {
+  count             = var.discvm_count 
   vpc_id            = aws_vpc.spcvpc.id
-  cidr_block        = var.discsubnet_cidr
-  availability_zone = data.aws_availability_zones.available.names[0]
-  tags   = merge(var.common_tags, { Name = "SUBNET-${var.reg_code}-SPC-STG-RUNR"})
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index) 
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = merge(var.common_tags, {
+    Name = "SUBNET-${var.reg_code}-SPC-STG-RUNR-${count.index + 1}"
+  })
 }
+
 
 resource "aws_subnet" "backsubnet" {
   vpc_id            = aws_vpc.spcvpc.id
