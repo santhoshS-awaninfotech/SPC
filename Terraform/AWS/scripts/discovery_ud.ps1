3705c344e6a58c4a67ca8bebde547a1f676efba8
Santhosh Sampath <Santhosh.Sampath@ivanti.com>
2026-03-23 17:24:16 +0530

    updated for aws 2 vms

--- Terraform/AWS/scripts/discovery_ud.ps1
+++ Terraform/AWS/scripts/discovery_ud.ps1
@@ -1,23 +1,23 @@
 <powershell>
 $ErrorActionPreference = "SilentlyContinue"
-
+Write-Output "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] script started" | Out-File C:\Userdata.log -Append
 #0 Define the new password
 $newPassword = ConvertTo-SecureString "${ADMIN_PASSWORD}" -AsPlainText -Force
 Set-LocalUser -Name "Administrator" -Password $newPassword
-Write-Output "Administrator password reset successfully"
+Write-Output "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] Administrator password reset successfully" | Out-File C:\Userdata.log -Append
 
 #1. Create users
 $password = ConvertTo-SecureString "${USERA_PASSWORD}" -AsPlainText -Force
 New-LocalUser -Name "userA" -Password $password -FullName "User A" -Description "Admin user created via script"
 # Add the user to Administrators group
 Add-LocalGroupMember -Group "Administrators" -Member "userA"
-write-output "userA completed"
+write-output "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] userA completed" | Out-File C:\Userdata.log -Append
 
 $password = ConvertTo-SecureString "${USERB_PASSWORD}" -AsPlainText -Force
 New-LocalUser -Name "userB" -Password $password -FullName "User B" -Description "Admin user created via script"
 # Add the user to Administrators group
 Add-LocalGroupMember -Group "Administrators" -Member "userB"
-write-output "userB completed"
+write-output "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] userB completed" | Out-File C:\Userdata.log -Append
 
 #2. Install Python
 if (-not (Get-Command python -ErrorAction SilentlyContinue)) {
@@ -26,7 +26,7 @@ $pythonInstaller = "$env:TEMP\python-installer.exe"
 Invoke-WebRequest -Uri $pythonUrl -OutFile $pythonInstaller
 
 Start-Process -FilePath $pythonInstaller -ArgumentList "/quiet InstallAllUsers=1 PrependPath=1" -Wait
-write-output "Python completed"
+write-output "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] Python installed" | Out-File C:\Userdata.log -Append
 }
 
 #3 Install VS Code
@@ -35,7 +35,10 @@ $vsInstaller = "$env:TEMP\vscode-installer.exe"
 Invoke-WebRequest -Uri $vsUrl -OutFile $vsInstaller
 
 Start-Process -FilePath $vsInstaller -ArgumentList "/VERYSILENT /NORESTART" -Wait
-Write-Output "VS Code installation completed"
+Write-Output "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] VS Code installation completed" | Out-File C:\Userdata.log -Append
 
-Rename-Computer -NewName "${HOSTNAME}" -Force -Restart
+Rename-Computer -NewName "${HOSTNAME}" -Force -Verbose
+Write-Output "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] Host Renamed" | Out-File C:\Userdata.log -Append
+Restart-Computer -Force
+
 </powershell>
