46a7b1557783f0fd823d68998cd2e2682ed7d604
Santhosh Sampath <Santhosh.Sampath@ivanti.com>
2026-03-27 18:54:10 +0530

    updated code for spc aws

--- Terraform/AWS/security.tf
+++ Terraform/AWS/security.tf
@@ -3,49 +3,63 @@ resource "aws_security_group" "backend_sg" {
   name   = "SG-${var.reg_code}-SPC-STG-UIDB"
   vpc_id = aws_vpc.spcvpc.id
   tags   = merge(var.common_tags, { Name = "SG-${var.reg_code}-SPC-STG-UIDB"})
-
-  ingress {
-    Name        = "TestName"
-    description = "Allow RDP from anywhere"
-    from_port   = 3389
-    to_port     = 3389
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-   ingress {
-    description = "Allow Postgres from VPC and public"
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["10.100.2.0/24", "0.0.0.0/0"] 
-  }
-
-  egress {
-    description = "Allow all outbound traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
+
+resource "aws_vpc_security_group_ingress_rule" "rdp_ingress_fe" {
+  security_group_id = aws_security_group.backend_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 3389
+  to_port           = 3389
+  ip_protocol       = "tcp"
+  description       = "Allow RDP from anywhere"
+  tags              = { Name = "RDP Rule" }
+
+}
+
+ resource "aws_vpc_security_group_ingress_rule" "postgres_ingress_fe" {
+  security_group_id = aws_security_group.backend_sg.id
+  cidr_ipv4         = "10.100.2.0/24"
+  from_port         = 5432
+  to_port           = 5432
+  ip_protocol       = "tcp"
+  description       = "Allow Postgres from VPC"
+  tags              = { Name = "Postgres Rule" }
+
+}
+
+resource "aws_vpc_security_group_egress_rule" "all_outbound_fe" {
+  security_group_id = aws_security_group.backend_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+  description       = "Allow all outbound traffic"
+  tags              = { Name = "OutboundRule" }
+
+}
+
 
 resource "aws_security_group" "discovery_sg" {
   name   = "SG-${var.reg_code}-SPC-STG-RUNR"
   vpc_id = aws_vpc.spcvpc.id
   tags   = merge(var.common_tags, { Name = "SG-${var.reg_code}-SPC-STG-RUNR"})
 
-  ingress {
-    from_port   = 3389
-    to_port     = 3389
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+}
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_vpc_security_group_ingress_rule" "rdp_ingress_be" {
+  security_group_id = aws_security_group.discovery_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 3389
+  to_port           = 3389
+  ip_protocol       = "tcp"
+  description       = "Allow RDP from anywhere"
+  tags              = { Name = "RDP Rule" }
+
+}
+
+resource "aws_vpc_security_group_egress_rule" "all_outbound_be" {
+  security_group_id = aws_security_group.discovery_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+  description       = "Allow all outbound traffic"
+  tags              = { Name = "OutboundRule" }
+
 }
