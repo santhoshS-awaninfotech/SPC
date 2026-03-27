7dfa22e3a14dfd7984967c5b6fe715f1b96bf17d
Santhosh Sampath <Santhosh.Sampath@ivanti.com>
2026-03-27 20:15:00 +0530

    updated code for spc aws

--- Terraform/AWS/vm_discovery.tf
+++ Terraform/AWS/vm_discovery.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "DiscVM" {
   ami           = data.aws_ami.windows.id
   instance_type = var.disc_instance_type
   key_name      = aws_key_pair.akp.key_name
-  tags          = merge(var.common_tags, { Name = "VM-${var.reg_code}-SPC-STG-RUNR-${upper(substr(data.aws_availability_zones.available.names[count.index], -2, 2))}-${count.index + 1}${var.use_spot ? "(SPOT)" : ""}"})
+  tags          = merge(var.common_tags, { Name = "VM-${var.reg_code}-SPC-STG-RUNR-${upper(substr(data.aws_availability_zones.available.names[count.index], -2, 2))}-${count.index + 1}${var.use_spot ? " (SPOT)" : ""}"})
   #subnet_id     = aws_subnet.discsubnet[count.index].id
   #vpc_security_group_ids = [aws_security_group.discovery_sg.id]
 
